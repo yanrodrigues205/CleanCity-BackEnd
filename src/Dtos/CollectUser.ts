46508415ee3a0108c1a7da6a5bea7758a4e0dcfa
yanrodrigues205 <yan.pablo205@gmail.com>
2024-04-30 09:29:00 -0300

    :sparkles: feat: insertion of collection profiles for users

---
+++ src/Dtos/CollectUser.ts
@@ -0,0 +1,8 @@
+interface CollectUser
+{
+    readonly name:string;
+    readonly cpf_cnpj:string;
+    readonly description:string;
+    readonly phone:string;
+}
+export default CollectUser;
---
+++ src/Models/CollectUserModel.ts
@@ -0,0 +1,61 @@
+import { database } from "../Database/Connection";
+import CollectUser from "../Dtos/CollectUser";
+import UsersModel from "./UsersModel";
+export default class CollectUserModel extends UsersModel
+{
+    constructor()
+    {
+        super();
+    }
+
+    protected async insertCollectUser(collectUser: CollectUser) : Promise<boolean>
+    {
+        try
+        {
+            const insert = await database.collectUser.create({
+                data:{
+                    name: collectUser.name,
+                    cfpCnpj: collectUser.cpf_cnpj,
+                    phone: collectUser.phone,
+                    description: collectUser.description
+                },
+                select:{
+                    id:true
+                }
+            });
+            console.log(insert);
+            if(insert)
+                return true;
+            else
+                return false;
+        }
+        catch(err)
+        {
+            console.error(err);
+            return false;
+        }
+    }
+
+
+    protected async cpfCnpjExists(cpf_cnpj : string) : Promise<boolean>
+    {
+        try
+        {
+            const verify = await database.collectUser.findUnique({
+                where:{
+                    cfpCnpj: cpf_cnpj
+                }
+            });
+
+            if(verify)
+                return false;
+            else
+                return true;
+        }
+        catch(err)
+        {
+            console.error(err);
+            return false;
+        }
+    }
+}
