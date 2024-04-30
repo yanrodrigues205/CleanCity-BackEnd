88befab5f1d411e30913a02520c82dce49da7cce
yanrodrigues205 <yan.pablo205@gmail.com>
2024-04-29 22:06:22 -0300

    :sparkles: feat: route authenticator

---
+++ src/Middleware/AuthMiddleware.ts
@@ -0,0 +1,74 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { database } from "../Database/Connection";
+
+export default class AuthMiddeware
+{
+    constructor()
+    {
+
+    }
+
+    public static Authentication()
+    {
+        return async(req: Request, res: Response, next: NextFunction) => {
+            const authHeader = req.headers.authorization;
+            if(!authHeader || !authHeader.startsWith('Bearer '))
+            {
+                return res.status(401).json({
+                    "message": "Para acessar esta ambiente você necessita estar logado no sistema!",
+                    "status": 401
+                });
+            }
+
+            const token = authHeader.substring(7);
+
+
+            let SECURITY_KEY :string = String(process.env.SECURITY_KEY);
+
+            if(SECURITY_KEY.length == 0 || !SECURITY_KEY)
+            {
+                console.error("Chave de segurança da API inexistente!");
+            }
+
+            let decode : any;
+            try
+            {
+                decode = await verify(token, SECURITY_KEY, {
+                    complete: true
+                });
+            }
+            catch(err)
+            {
+                 return res.status(401).json({
+                    "message": "Sua sessão é inválida, tente logar novamente!",
+                    "status": 401
+                });
+            }
+
+            if(!decode.payload.id || !decode)
+            {
+                return res.status(401).json({
+                    "message": "Sua sessão é inválida, tente logar novamente!",
+                    "status": 401
+                });
+            }
+
+            const verifyIdUser = await database.users.findUnique({
+                where:{
+                    id: decode.payload.id
+                }
+            });
+
+            if(!verifyIdUser)
+            {
+                return res.status(401).json({
+                    "message": "Sua sessão é inválida, tente logar novamente!",
+                    "status": 401
+                });
+            }
+
+            return next();
+    };
+    }
+}
