856ee2112ae41a1d477da1bfa6eddabb04fc94d4
yanrodrigues205 <yan.pablo205@gmail.com>
2024-08-06 07:19:46 -0300

    :sparkles: feat: dividing session creation into two steps and adding verification method

--- src/Controllers/SessionsController.ts
+++ src/Controllers/SessionsController.ts
@@ -1,6 +1,10 @@
 import { compare } from "bcryptjs";
 import Session from "../Dtos/Session";
 import SessionsModel from "../Models/SessionsModel";
+import OTP from "../@Types/OTP";
+import axios from 'axios';
+import AuthMiddeware from "../Middleware/AuthMiddleware";
+
 
 export default class SessionsController extends SessionsModel
 {
@@ -10,7 +14,7 @@ export default class SessionsController extends SessionsModel
     }
 
 
-    public async createSession(req: any, res: any)
+    public async firstFactor(req: any, res: any)
     {
         const data: Session = req.body;
 
@@ -22,13 +26,47 @@ export default class SessionsController extends SessionsModel
                     });
         }
 
+        if(!data.recaptcha)
+        {
+            return  res.status(402).json({
+                message: "Para concluir a authenticação é necessário o preenchimento do reCAPTCHA.",
+                status: 402
+            });
+        }
+
+        const recaptcha_secret = String(process.env.RECAPTCHA_SECRET);
+        try
+        {
+            const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+                params: {
+                    secret: recaptcha_secret,
+                    response: data.recaptcha
+                }
+            });
+
+            const response_data = response.data.success;
+
+            if(!response_data)
+            {
+                return  res.status(402).json({
+                    message: "Erro de validação do reCAPTCHA, tente novamente.",
+                    status: 402
+                });
+            }
+        }
+        catch(err)
+        {
+            console.log(err);
+            return;
+        }
+
         const verifyEmail : any = await super.verifyEmail(data.email);
 
         if(!verifyEmail)
         {
-            return  res.status(400).json({
+            return  res.status(401).json({
                         message: "O email não foi encontrado no sistema, digite novamente!",
-                        status: 400
+                        status: 401
                     });
         }
 
@@ -36,23 +74,93 @@ export default class SessionsController extends SessionsModel
 
         if(!comparePass)
         {
-            return  res.status(400).json({
+            return  res.status(403).json({
                         message: "A senha digitada esta incorreta, digite novamente!",
+                        status: 403
+                    });
+        }
+
+        const firstFactorSend : OTP | false = await super.twoFactors_sendCode(verifyEmail.email, verifyEmail.id);
+
+        if(!firstFactorSend)
+        {
+            return res.status(421).json({
+                message: "Não foi possível concluir o o primeiro passo da authenticação dois fatores.",
+                status: 421
+            });
+        }
+
+        return res.status(202).json({
+            message: "Para concluir a authenticação, verifique a caixa de emails aonde foi enviado o código de verificação.",
+            id_OTP: firstFactorSend.id,
+            expiry: firstFactorSend.expiry,
+            status: 202
+        });
+    }
+
+    public async secondFactor(req: any, res: any)
+    {
+        const data = req.body;
+
+        if(!data.otp || !data.id || data.otp.length != 6)
+        {
+            return  res.status(400).json({
+                        message: "Indentificação de fatores faltando campos, tente novamente.",
                         status: 400
                     });
         }
 
+        const secondFactorVerify = await super.twoFactors_verifyCode(data.id, data.otp);
 
-        const insertSession = await super.insert(verifyEmail);
-
-        if(insertSession)
+        if(!secondFactorVerify)
         {
-            return  res.status(202).json({
-                        message: "Sessão iniciada com sucesso!",
-                        token: insertSession,
-                        status: 202
+            return  res.status(401).json({
+                message: "Indentificação de fatores inválida, tente novamente.",
+                status: 401
+            });
+        }
+
+
+        const insertSession = await super.insert(data.id);
+
+        if(!insertSession)
+        {
+            return  res.status(402).json({
+                message: "Não foi possível realizar a criação da sessão!",
+                status: 402
+            });
+        }
+
+        return  res.status(202).json({
+            message: "Sessão iniciada com sucesso!",
+            token: insertSession,
+            status: 202
+        });
+    }
+
+
+    public async verifySession(req: any, res: any)
+    {
+        const data = req.body;
+
+        if(!data.token)
+        {
+            return  res.status(400).json({
+                        message: "Para a verificação de sessão é necessário informar o token.",
+                        status: 400
                     });
         }
-        
+
+        let response = await AuthMiddeware.verifyRoute(data.token);
+
+        if(!response)
+        {
+            return  res.status(401).json({
+                message: "Não foi possível decodificar o token.",
+                status: 401
+            });
+        }
+
+        return res.status(202).json(response);
     }
 }
--- src/Models/SessionsModel.ts
+++ src/Models/SessionsModel.ts
@@ -1,52 +1,86 @@
 import Users from "../@Types/Users";
 import {sign, verify } from "jsonwebtoken";
 import { database } from "../Database/Connection";
-export default class SessionsModel
+import TwoFactorsModel from "./TwoFactorsModel";
+import OTP from "../@Types/OTP";
+export default class SessionsModel extends TwoFactorsModel
 {
     constructor()
     {
-
+        super();
     }
 
-
-    protected async insert(users: any)
+    protected async twoFactors_sendCode(email: string, user_id: string): Promise<OTP | false>
     {
-       
-            const dataExpiracao = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-            let SECURITY_KEY :string = String(process.env.SECURITY_KEY);
+        //OTP = One-Time Password
+        const sendOTP : OTP | false = await super.create(email, user_id);
 
-            if(SECURITY_KEY.length == 0 || !SECURITY_KEY)
-            {
-                console.error("Chave de segurança da API inexistente!");
-            }
+        if(sendOTP)
+        {
+            return sendOTP;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected async twoFactors_verifyCode(id: string, OTP_receiver: string) : Promise<Boolean>
+    {
+        const verifyOTP = await super.verify(id, OTP_receiver);
+
+        if(verifyOTP)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+  
 
 
-            let create_token;
-            try
-            {
-                create_token = await sign(users, SECURITY_KEY, {
-                    algorithm: "HS256",
-                    expiresIn: "24h"
-                });
-            }
-            catch(err)
-            {
-                console.error(err);
-                return false;
-            }
+    protected async insert(id_twofactors: string)
+    {
+        const dataExpiracao = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+        let SECURITY_KEY :string = String(process.env.SECURITY_KEY);
 
-            const insert = await database.sessions.create({
-                data:{
-                    token: create_token,
-                    expiry: dataExpiracao,
-                    user_id: users.id
-                }
+        if(SECURITY_KEY.length == 0 || !SECURITY_KEY)
+        {
+            console.error("Chave de segurança da API inexistente!");
+        }
+
+        let create_token;
+        let payload = {
+            id_twofactors
+        };
+        try
+        {
+            create_token = await sign(payload, SECURITY_KEY, {
+                algorithm: "HS256",
+                expiresIn: "24h"
             });
+        }
+        catch(err)
+        {
+            console.error(err);
+            return false;
+        }
 
-            if(insert)
-                return create_token;
-            else
-                return false;
+
+        const insert = await database.sessions.create({
+            data:{
+                token: create_token,
+                expiry: dataExpiracao,
+                twofactors_id: id_twofactors
+            }
+        });
+
+        if(insert)
+            return create_token;
+        else
+            return false;
     }
 
     protected async verifyEmail(email: string)
